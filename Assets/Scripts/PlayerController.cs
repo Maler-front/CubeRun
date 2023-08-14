676a6f4b142770a5e0c8072b3cbedd99b947b9f8
MrGooD <mrgood1998@mail.ru>
2023-08-14 19:04:27 +0300

    Changed the motion for mobile control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,77 +5,73 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rb;
-
-    public float Speed;                                //Скорость
-    public float Pivot;                                //Сила поворота
-    public float JumpHeight;                           //Сила прыжка
-
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _speed;                                //Скорость
+    [SerializeField] private float _pivot;                                //Сила поворота
+    [SerializeField] private float _jumpHeight;                           //Сила прыжка
+    [SerializeField] private GameObject _player;
+    private float screen;
     private bool isRoad;
                                                        //Проверка на нахождении в воздухе
     private void OnCollisionEnter(Collision collision) //На земле
     {
-        if (collision.gameObject.tag == "Road")
-        {
+        if (collision.gameObject.tag == "Road" || collision.gameObject.CompareTag("Road"))
             isRoad = true;
-        }
     }
 
     private void OnCollisionExit(Collision collision) //Летим
     {
-        if (collision.gameObject.tag == "Road")
-        {
+        if (collision.gameObject.tag == "Road" || collision.gameObject.CompareTag("Road"))
             isRoad = false;
+    }
+
+    private void Start()
+    {
+        screen = Screen.width;
+        _rigidbody = _player.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        int i = 0;
+        
+        while (i < Input.touchCount && i < 2)
+        {
+            if (Input.GetTouch(i).position.x > screen / 2 )
+                Move(1.0f);
+            else if (Input.GetTouch(i).position.x < screen / 2)
+                Move(-1.0f);
+
+            ++i;
         }
     }
 
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, Speed * Time.deltaTime); //Ускорение вперед
+        _rigidbody.AddForce(0, 0, _speed * Time.deltaTime); //Ускорение вперед
 
         if (Input.GetKey("a"))                     //Движение влево
-        {
             Move(-1.0f);
-        }
-        if (Input.GetKey("d"))                     //Движение вправо
-        {
+        else if (Input.GetKey("d"))                     //Движение вправо
             Move(1.0f);
-        }
+
         if (isRoad)                                //Если на земле
         {
             if (Input.GetKey("e"))                 //Прыгаем на "е"
-            {
                 Jump();
-            }
 
-            if (Input.touchCount == 2)             //Прыгаем на два касания
-            {
+            else if (Input.touchCount == 2)             //Прыгаем на два касания
                 Jump();
-            }
         }
-
-        OnLeftBottonDown();                        //Двигаемься влево
-        OnRightBottonDown();                       //Двигаемься вправо
-
-    }
-
-    public void OnLeftBottonDown()                 //Кнопка на движение влево при нажатии
-    {
-        Move(-1.0f);
-    }
-
-    public void OnRightBottonDown()                //Кнопка на движение вправо при нажатии
-    {
-        Move(1.0f);
     }
 
     private void Jump()                            //Метод на прыжок
     {
-        rb.AddForce(0, JumpHeight * Time.deltaTime, 0, ForceMode.VelocityChange);
+        _rigidbody.AddForce(0, _jumpHeight * Time.deltaTime, 0, ForceMode.VelocityChange);
     }
 
     private void Move(float HorizontalMovement)   //Метод на скорость 
     {
-        rb.AddForce(HorizontalMovement * Pivot * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        _rigidbody.AddForce(HorizontalMovement * _pivot * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
     }
 }
