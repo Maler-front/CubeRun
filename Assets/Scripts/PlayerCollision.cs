a5539cd551be07a936478a917572379d0bb7a6e3
MrGooD <mrgood1998@mail.ru>
2023-09-11 20:20:13 +0300

    Changed the condition of appearance of the victory panel

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public GameObject completeLevelUI;
     public PlayerController movement;
 
     private float _timeLeft = 0.7f;
@@ -21,7 +22,7 @@ public class PlayerCollision : MonoBehaviour
 
     private void Update()
     {
-        if(!movement.enabled)
+        if(!movement.enabled && completeLevelUI)
         {
             _timeLeft -= Time.deltaTime;
 
